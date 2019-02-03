540cbe01a5d0b0add143c39a91f50578ae1e1640
GrantBravo29 <league@WTS-IM-17.attlocal.net>
2019-02-03 11:30:17 -0800

    some stuff such as a muted tomato

---
+++ src/ifs_ints_and_loops/badger.java
@@ -0,0 +1,40 @@
+package ifs_ints_and_loops;
+
+import javax.swing.JOptionPane;
+
+public class badger {
+public static void main(String[] args) {
+String badger = "badger";
+String MUSHROOM = "MUSHROOM";
+for (int i = 0; i < 16; i++) {
+		JOptionPane.showMessageDialog(null, "badger");
+	}
+for (int i = 0; i < 2; i++) {JOptionPane.showMessageDialog(null, "MUSHROOM");
+	 for (int j = 0; j < 16; j++) {JOptionPane.showMessageDialog(null, "badger");
+		
+	}
+	 for (int j = 0; j < 2; j++) {
+		 JOptionPane.showMessageDialog(null, "MUSHROOM");
+		for (int j2 = 0; j2 < 16; j2++) {
+			JOptionPane.showMessageDialog(null, "badger");
+			
+		}
+	for (int j2 = 0; j2 < 2; j2++) {
+		JOptionPane.showMessageDialog(null, "MUSHROOM");
+		
+	}
+for (int j2 = 0; j2 < 2; j2++) {
+	JOptionPane.showMessageDialog(null, "A SNAKE");
+	
+}
+for (int j2 = 0; j2 < 2; j2++) {
+	JOptionPane.showMessageDialog(null, "SNAAAKE");
+}		
+	JOptionPane.showMessageDialog(null, "Ooo SNAKE");
+	 }
+	 
+	 }
+	 
+}
+}
+
---
+++ tomato_rat/tomato_rat.pde
@@ -0,0 +1,34 @@
+void setup() {
+    size(500, 500);
+}
+void draw() {
+    background(200, 200, 200);
+ 
+    noStroke();
+    
+ 
+       fill(#ff0000);
+    ellipse(150, 200, 150, 150);
+    ellipse(212, 200, 150, 150);
+      fill(#2cc402);
+    rect(176, 103, 12, 32);
+if(mousePressed){
+fill(200,200,200);
+   ellipse(180,225,75,75);
+fill(#fffdfb);
+ellipse(140,160,50,50);
+fill(#fffdfb);
+ellipse(225,160,50,50);
+fill(#030303);
+ellipse(140,160,25,25);
+fill(#030303);
+ellipse(225,160,25,25);
+fill(#f0b7dd);
+rect(275,225,100,15);
+fill(#ffb700);
+noStroke();
+rect(150,200,60,60);
+fill(#edaa00);
+ellipse(
+}
+}
